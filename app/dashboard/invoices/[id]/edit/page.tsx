6a7e35673ae2d96866e73045d607624e848c68ef
Ignacio Lencina <nacholencina60@gmail.com>
2025-10-06 17:16:11 -0300

    Up to chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchCustomers } from "@/app/lib/data";
-import { fetchInvoiceById } from "@/app/lib/data";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function EditInvoicePage(props: {
   params: Promise<{ id: string }>;
@@ -12,7 +12,11 @@ export default async function EditInvoicePage(props: {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-  
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
